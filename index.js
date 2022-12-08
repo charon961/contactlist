9b80b35df480647bee9449c5796972ec41d38424
charon961 <76212266+charon961@users.noreply.github.com>
2022-12-09 04:18:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,24 +2,41 @@ const port=3000;
 const express=require('express');
 const path =require('path');
 
+const body=require('body-parser');
+const { param } = require('express/lib/request');
+
 const app=express();
 app.set('view engine','ejs');
 
-contactList=[
-  {
-     name:"yash",
-     phone:"1212112"
-  }
-];
+app.use("/public", express.static(__dirname + '/public'));
+app.use(body.urlencoded({extended:false}));
+contactList=[];
 app.set('views',path.join(__dirname,'views'))
 app.get("/",function(req,res){
-    return res.render('home',{title:"Hello World!!", contactList:contactList});
+    return res.render('home',{title:"Contact List", contactList:contactList});
 })
+
+app.get("/delete-contact/:phone",function(req,res){
+   let number=req.params.phone;
+  let index=contactList.findIndex(f=>f.phone==number);
+    if(index!=-1){
+      contactList.splice(index,1);
+    }
+    return res.redirect("/");
+})
+
+
+app.post("/",function(req,res){
+    contactList.push(req.body);
+    return  res.redirect("/");
+})
+
+
 
 
 app.listen(port,function(err){
       if(err){
-      
+         console.log("Error!!!");
       }
       else{
         console.log("server is running");
